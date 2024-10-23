e9a17f70dde68a4b6829226f30351494cfdab21b
PRATHAMGADHIYA <154453177+PRATHAMGADHIYA@users.noreply.github.com>
2024-10-23 13:28:26 +0530

    Node

--- HTTP-SERVER-PR-1/project/index.js
+++ HTTP-SERVER-PR-1/project/index.js
@@ -5,8 +5,7 @@ const server = http.createServer((req, res) => {
     if (req.url == "/") {
         fs.readFile("index.html", (err, data) => {
             if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end("Error loading page");
+               console.log(err)
             } else {
                 res.writeHead(200, { "Content-Type": "text/html" });
                 res.end(data);
@@ -24,9 +23,6 @@ const server = http.createServer((req, res) => {
     } else if (req.url === "/service") {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end("welcome to the service page");
-    } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("404 Not Found");
     }
 });
 
